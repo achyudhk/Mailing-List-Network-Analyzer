8ce4aa320286d73f5041cbefd6874b91baf1d9bc
Achyudh K Ram <achyudhk@gmail.com>
2017-08-27 19:39:47 +0530

    Update Docs to sync with Upstream

--- data/sakai-devel/scripts/combine.sh
+++
@@ -1,3 +0,0 @@
-for f in ./data/*;
-do (cat "${f}"; echo) >> sakai-devel.mbox; 
-done
--- data/sakai-devel/scripts/fetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#script to fetch sakai.devel mailing list archives
-
-wget --wait=1 --random-wait --limit-rate=1M --input-file=urls
-
